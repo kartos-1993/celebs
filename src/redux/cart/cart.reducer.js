bb38a470417c770bd634c4fd7a97925c6f55ff52
kartos-1993 <69756720+kartos-1993@users.noreply.github.com>
2021-01-20 19:56:11 +0545

    add remove cart items feature from checkout item

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,5 +1,5 @@
 import CartActionTypes from './cart.types'
-import { addItemToCart, removeItemFromCart } from './cart.util'
+import { addItemToCart } from './cart.util'
 
 const INITIAL_STATE = {
     hidden: true,
