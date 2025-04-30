a54740be1be712d043cec9f71619cff692160c40
kartos-1993 <1993kartos@gmail.com>
2025-04-30 23:02:49 +0545

    fix(asyncHandler): correct type name and enhance error handling feat(tsconfig): update target to ES2021 for improved compatibility

--- apps/admin/auth-api/src/middlewares/asyncHandler.ts
+++ apps/admin/auth-api/src/middlewares/asyncHandler.ts
@@ -1,7 +1,17 @@
 import { Request, Response, NextFunction } from 'express';
 
-type AsyncControlerType = (
+type AsyncControllerType = (
   req: Request,
   res: Response,
   next: NextFunction
 ) => Promise<any>;
+
+export const asyncHandler =
+  (controller: AsyncControllerType): AsyncControllerType =>
+  async (req, res, next) => {
+    try {
+      await controller(req, res, next);
+    } catch (error) {
+      next(error);
+    }
+  };
