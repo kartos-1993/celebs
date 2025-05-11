768fa45c82b1c3fabaab976962d878b2614bc7b2
kartos-1993 <1993kartos@gmail.com>
2025-05-11 13:13:49 +0545

    feat(exceptions): implement custom exception classes for error handling

---
+++ apps/admin/auth-api/src/common/utils/jwt.ts
@@ -0,0 +1,55 @@
+import { ErrorCode } from '../enums/error-code.enum';
+import { HTTPSTATUS, HttpStatusCode } from '../../config/http.config';
+import { AppError } from './AppError';
+
+export class NotFoundException extends AppError {
+  constructor(message = 'Resource not found', errorCode?: ErrorCode) {
+    super(
+      message,
+      HTTPSTATUS.NOT_FOUND,
+      errorCode || ErrorCode.RESOURCE_NOT_FOUND
+    );
+  }
+}
+
+export class BadRequestException extends AppError {
+  constructor(message = 'Bad Request', errorCode?: ErrorCode) {
+    super(message, HTTPSTATUS.BAD_REQUEST, errorCode);
+  }
+}
+
+export class UnauthorizedException extends AppError {
+  constructor(message = 'Unauthorized Access', errorCode?: ErrorCode) {
+    super(
+      message,
+      HTTPSTATUS.UNAUTHORIZED,
+      errorCode || ErrorCode.ACCESS_UNAUTHORIZED
+    );
+  }
+}
+
+export class InternalServerException extends AppError {
+  constructor(message = 'Internal Server Error', errorCode?: ErrorCode) {
+    super(
+      message,
+      HTTPSTATUS.INTERNAL_SERVER_ERROR,
+      errorCode || ErrorCode.INTERNAL_SERVER_ERROR
+    );
+  }
+}
+
+export class TooManyRequestsException extends AppError {
+  constructor(message: string, code?: ErrorCode) {
+    super(message, HTTPSTATUS.TOO_MANY_REQUESTS, code);
+  }
+}
+
+export class HttpException extends AppError {
+  constructor(
+    message = 'Http Exception Error',
+    statusCode: HttpStatusCode,
+    errorCode?: ErrorCode
+  ) {
+    super(message, statusCode, errorCode);
+  }
+}
