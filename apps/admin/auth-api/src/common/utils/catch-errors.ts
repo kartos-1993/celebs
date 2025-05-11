868532cefe55bebe24954806bb8d417df95b32c1
kartos-1993 <1993kartos@gmail.com>
2025-05-11 13:13:16 +0545

    fix(errors): standardize error messages and improve import order in catch-errors utility

--- apps/admin/auth-api/src/common/utils/catch-errors.ts
+++ apps/admin/auth-api/src/common/utils/catch-errors.ts
@@ -1,9 +1,9 @@
-import { HTTPSTATUS, HttpStatusCode } from '../../config/http.config';
 import { ErrorCode } from '../enums/error-code.enum';
+import { HTTPSTATUS, HttpStatusCode } from '../../config/http.config';
 import { AppError } from './AppError';
 
 export class NotFoundException extends AppError {
-  constructor(message = 'Resource Not Found', errorCode?: ErrorCode) {
+  constructor(message = 'Resource not found', errorCode?: ErrorCode) {
     super(
       message,
       HTTPSTATUS.NOT_FOUND,
@@ -13,7 +13,7 @@ export class NotFoundException extends AppError {
 }
 
 export class BadRequestException extends AppError {
-  constructor(message = 'BadRequest', errorCode?: ErrorCode) {
+  constructor(message = 'Bad Request', errorCode?: ErrorCode) {
     super(message, HTTPSTATUS.BAD_REQUEST, errorCode);
   }
 }
@@ -25,5 +25,31 @@ export class UnauthorizedException extends AppError {
       HTTPSTATUS.UNAUTHORIZED,
       errorCode || ErrorCode.ACCESS_UNAUTHORIZED
     );
+  }
+}
+
+export class InternalServerException extends AppError {
+  constructor(message = 'Internal Server Error', errorCode?: ErrorCode) {
+    super(
+      message,
+      HTTPSTATUS.INTERNAL_SERVER_ERROR,
+      errorCode || ErrorCode.INTERNAL_SERVER_ERROR
+    );
+  }
+}
+
+export class TooManyRequestsException extends AppError {
+  constructor(message: string, code?: ErrorCode) {
+    super(message, HTTPSTATUS.TOO_MANY_REQUESTS, code);
+  }
+}
+
+export class HttpException extends AppError {
+  constructor(
+    message = 'Http Exception Error',
+    statusCode: HttpStatusCode,
+    errorCode?: ErrorCode
+  ) {
+    super(message, statusCode, errorCode);
   }
 }
