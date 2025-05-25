c1a0860f10684998fef3c6bf733fd076640f04ce
kartos-1993 <1993kartos@gmail.com>
2025-05-25 21:49:14 +0545

    refactor: remove unused imports and loader data from AppLayout component

--- apps/admin/web/src/layout/AppLayout.tsx
+++ apps/admin/web/src/layout/AppLayout.tsx
@@ -1,14 +1,11 @@
 import SideBar from '@/components/sidebar';
-import SideBarProvider, { useSideBarContext } from '@/context/sidebar-provider';
-import { cn } from '@/lib/utils';
-import { Outlet, useLoaderData, useRouteLoaderData } from 'react-router-dom';
-import { ProtectedLoaderData } from '@/types';
+import SideBarProvider from '@/context/sidebar-provider';
+import { Outlet } from 'react-router-dom';
+
 import Main from '@/components/main';
 import { Navbar } from '@/components/nav-bar';
 
 const AppLayout = () => {
-  const data = useLoaderData() as ProtectedLoaderData;
-
   return (
     <div>
       <SideBarProvider>
