72667859b95d86cb07ab7458af70091d74be1845
kartos-1993 <1993kartos@gmail.com>
2026-07-25 12:14:25 +0545

    refactor(shared-utils): optimize asyncHandler using Promise.resolve().catch(next) pattern

--- packages/shared-utils/src/middlewares/asyncHandler.ts
+++ packages/shared-utils/src/middlewares/asyncHandler.ts
@@ -1,11 +1,7 @@
 import { Request, Response, NextFunction, RequestHandler } from 'express';
 
-export const asyncHandler = (fn: RequestHandler): RequestHandler => {
-  return async (req: Request, res: Response, next: NextFunction) => {
-    try {
-      await fn(req, res, next);
-    } catch (error) {
-      next(error);
-    }
+export const asyncHandler =
+  (fn: RequestHandler): RequestHandler =>
+  (req: Request, res: Response, next: NextFunction) => {
+    Promise.resolve(fn(req, res, next)).catch(next);
   };
-};
